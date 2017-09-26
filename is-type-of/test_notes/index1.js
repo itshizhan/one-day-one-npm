334f4ebc7fe1f0145ba0d8739af1346a5a621c88
leeson8888 <251076355@qq.com>
2017-09-27 00:42:32 +0800

    pls ignore

--- is-type-of/test_notes/index1.js
+++ is-type-of/test_notes/index1.js
@@ -6,12 +6,44 @@ var fn = function(){
 var fn2 = function* (){
 
 }
-assert(is.function(fn2), 'app.use() requires a function');
+// assert(is.function(fn2), 'app.use() requires a function');
 
-console.log(is.NaN(NaN) === true);
+// console.log(is.NaN(NaN) === true);
 
-class F {}
-function G() {}
 
-console.log(is.class(F)); // true
-console.log(is.class(G)); // false
+
+// console.log(is.class(F)); // true
+// console.log(is.class(G)); // false
+function Log(str){
+    console.log(str);
+}
+// Log(F.toString());
+
+// Log(G.toString());
+
+function Log(str){
+    console.log(str);
+}
+class A {}
+function B() {}
+var C = [2,3];
+var D = new Date();
+
+var objToString = Object.prototype.toString;
+var fnToString = Function.prototype.toString;
+
+Log(objToString.call(A));
+Log(fnToString.call(A));
+
+Log(objToString.call(B));
+Log(fnToString.call(B));
+
+Log(objToString.call(C));
+Log(objToString.call(D));
+
+// [object Function]
+// class A {}
+// [object Function]
+// function B() {}
+// [object Array]
+// [object Date]
